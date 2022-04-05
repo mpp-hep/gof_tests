57b9097f1d261603bf623a8c8ac9d4e06f8bab2d
Thea Aarrestad <thea.aarrestad@cern.ch>
2022-04-05 15:50:57 +0200

    Adding code to run combine

--- run_combine.py
+++ run_combine.py
@@ -42,41 +42,32 @@ def makeWS(data_rej,data_acc,signal_rej,signal_acc, outname,quantile,qr_unc_file
   for iBinX in range(1,data_rej.GetNbinsX()+1):
       empty_hist.SetBinContent(iBinX,1)
       rej_bin = data_rej.GetBinContent(iBinX)
-     # Name efficiency per quantile to allow for combination 
-      # w.factory('eff_%s_%i[%f,%f,%f]'%(quantile,iBinX,efficiency,efficiency*0.98,efficiency*1.02,))
-#       w.var('eff_%s_%i'%(quantile,iBinX)).setConstant(True)
       
-      # Uncertainty on yield in reject based on QR envelope: qr_rms/qr_cutValue
+     # If adding a per-bin uncertianty on the CR or the efficiency estimate
+      # w.factory('eff_%s_%i[%f,%f,%f]'%(quantile,iBinX,efficiency,efficiency*0.98,efficiency*1.02,))
+      # w.var('eff_%s_%i'%(quantile,iBinX)).setConstant(True)
+      
+      # WIP that was abandoned: Adding Uncertainty on yield in reject based on QR envelope: qr_rms/qr_cutValue. Use function option qr_unc_file=None which needs a json file as input
 #       rms_qr = qr_unc_file['q90'][iBinX-1][2]/qr_unc_file['q90'][iBinX-1][1]
-#       rms_qr = 0.064
 #       print('crBin%i_In[%.1f,%.1f,%.1f]'%(iBinX,rej_bin,rej_bin*(1-rms_qr),rej_bin*(1+rms_qr)))
 #       w.factory('crBin%i_In[%.1f,%.1f,%.1f]'%(iBinX,rej_bin,rej_bin*(1-rms_qr),rej_bin*(1+rms_qr)))
-      # w.factory('crBin%i_In[%.1f,%.1f,%.1f]'%(iBinX,rej_bin,rej_bin-rej_bin**0.5,rej_bin+rej_bin**0.5))
+
+      
       w.factory('crBin%i_In[%.1f]'%(iBinX,rej_bin))
-      # Fix q=0.9-1.0 bin count
+      # Fix control region bin count
       w.var('crBin%i_In'%iBinX).setConstant(True)
-      #Name per-bin background estimate per quantile to allow for combination
+      
       w.factory('crBin_q%s_%i[0,-10,10]'%(quantile,iBinX))
       w.var('crBin_q%s_%i'%(quantile,iBinX)).setConstant(False)
       if rej_bin !=  0.:
         power = 1/rt.TMath.Sqrt(rej_bin)
       else:
         power = -1.0
-        # w.var('crBin%i'%iBinX).setConstant(True)
+
         w.var('crBin_q%s_%i'%(quantile,iBinX)).setConstant(True)
+        
       #what is fit is actually (mjj+mjj/sqrt(mjj))^x_bin, meaning x_bin is representative of how many sigma acc is from rej
       w.factory("expr::crBin%iFunc_q%s('max(0,@0*pow(1.0+%f,@1))',crBin%i_In,crBin_q%s_%i)"%(iBinX,quantile,power,iBinX,quantile,iBinX))
-#       effDown = efficiency*(1-0.063)
-#       effUp   = efficiency*(1+0.063)
-#       w.factory('eff%i[%f,%f,%f]'%(iBinX,efficiency,effDown,effUp))
-
-#       if data_rej.GetBinContent(iBinX) !=  0.:
-#         w.var('eff%i'%iBinX).setConstant(False)
-#       else:
-#          w.var('eff%i'%iBinX).setConstant(True)
-#       w.factory("expr::bin%iFunc('max(0,@0*@1)',eff%i,crBin%iFunc)"%(iBinX,iBinX,iBinX))
-    
-      # w.factory("expr::bin%iFunc('max(0,@0*@1)',eff_%s_%i,crBin%iFunc)"%(iBinX,quantile,iBinX,iBinX))
       w.factory("expr::bin%iFunc_q%s('max(0,@0*@1)',eff_%s,crBin%iFunc_q%s)"%(iBinX,quantile,quantile,iBinX,quantile))
       rej_bin_functions.add(w.function('crBin%iFunc_q%s'%(iBinX,quantile)))
       acc_bin_functions.add(w.function('bin%iFunc_q%s'%(iBinX,quantile)))
@@ -125,12 +116,10 @@ def makeWS(data_rej,data_acc,signal_rej,signal_acc, outname,quantile,qr_unc_file
   lumi lnN 1.01 -
   '''
   datacard_ratio += 'eff_%s   flatParam\n'%quantile
-  # for i in range(1,n_bins+1):
-  #     datacard_ratio += 'eff_%s_%i   flatParam\n'%(quantile,i)
+  # for i in range(1,n_bins+1): #If you have bin-ny-bin uncertianties on the QR efficiency
+  #     datacard_ratio += 'eff_%s_%i   flatParam\n'%(quantile,i) 
   for i in range(1,n_bins+1):
       datacard_ratio += 'crBin_q%s_%i   flatParam\n'%(quantile,i)
-#   for i in range(1,n_bins+1):
-#       datacard_ratio += 'crBin%i_In   flatParam\n'%i
   # write datacard
   datacard_ratio_acc = datacard_ratio.format(BIN='acc',
                             OBS=-1,
@@ -153,20 +142,26 @@ def makeWS(data_rej,data_acc,signal_rej,signal_acc, outname,quantile,qr_unc_file
   os.system('combineCards.py rej={REJ} acc={ACC} > {RATIO}'.format(REJ=outname.replace('.root','_rej.txt'),ACC=outname.replace('.root','_acc.txt'),RATIO=outname.replace('.root','_ratio.txt')))
   
 if __name__ == "__main__":
-
+  
+  # Which crossection you are running the GOF for
   xsecs   = [0,10,20,40,60,80,100]
   xsecs   = [0]
   
-  columns = {0.5: 21, 0.1: 18, 0.3: 22, 0.9: 17, 0.7: 20, 0.01: 19}
-  columns = {0.01: 19, 0.1: 18, 0.3: 22,0.5: 21, 0.7: 20}
+  # Which quantiles to run GOF for
+  #columns = {0.5: 21, 0.1: 18, 0.3: 22, 0.9: 17, 0.7: 20, 0.01: 19}
+  # columns = {0.01: 19, 0.1: 18, 0.3: 22,0.5: 21, 0.7: 20}
+  columns = {0.01: 19, 0.1: 18, 0.3: 22,0.5: 21}
   
-  columns = {0.7: 20}
-  
+  # RUn combine on all quantiles
   doAllQuantiles = True
+  
+  # You have already run the above, and now want to combine the quantiles
   combineAll     = False
-
+  
+  # Informative name for the Combine outputs
   tag = "sig_GtoWW35naReco"  
   
+  NTOYS = 5 # Multiplied by 10, running in a i=10 loop with different seed each time  
   if doAllQuantiles:
   
     for xs in xsecs:
@@ -200,8 +195,8 @@ if __name__ == "__main__":
             prefix = "q{}_xs{}_{}".format(quantile,xs,tag)
             os.system('combine -M GoodnessOfFit --algo saturated --fixedSignalStrength 0 -d datacard_ws_{PREFIX}_ratio.txt -n Ratio_gof_{PREFIX} --dataset data_obs -v 2'.format(PREFIX=prefix))
 
-            for i in range(1):
-              os.system('combine -M GoodnessOfFit --algo saturated --fixedSignalStrength 0 -d datacard_ws_{PREFIX}_ratio.txt -t 50 --toysFreq -n Ratio_gof_toys_{PREFIX}  --dataset data_obs -s {S} -v 0'.format(PREFIX=prefix,S=40+i))
+            for i in range(10):
+              os.system('combine -M GoodnessOfFit --algo saturated --fixedSignalStrength 0 -d datacard_ws_{PREFIX}_ratio.txt -t {NTOYS} --toysFreq -n Ratio_gof_toys_{PREFIX}  --dataset data_obs -s {S} -v 0'.format(PREFIX=prefix,NTOYS=NTOYS,S=40+i))
             os.system('hadd -f higgsCombineRatio_gof_toys_{PREFIX}.GoodnessOfFit.mH120.ALLTOYS.root higgsCombineRatio_gof_toys_{PREFIX}.GoodnessOfFit.mH120.4*.root'.format(PREFIX=prefix))
 
     for xs in xsecs:
@@ -213,8 +208,6 @@ if __name__ == "__main__":
             # open file
             obs_gof_file = uproot.open('higgsCombineRatio_gof_{PREFIX}.GoodnessOfFit.mH120.root'.format(PREFIX=prefix))
             obs_gof = obs_gof_file['limit'].arrays('limit')['limit'][0]
-
-            
 
             exp_gof_file = uproot.open('higgsCombineRatio_gof_toys_{PREFIX}.GoodnessOfFit.mH120.ALLTOYS.root'.format(PREFIX=prefix))
             exp_gof = exp_gof_file['limit'].arrays('limit')['limit']
